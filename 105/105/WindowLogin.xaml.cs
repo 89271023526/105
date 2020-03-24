32caaa0b7d9aa56e948b8d9c51d75aa65fe40029
ирина <60813676+89271023526@users.noreply.github.com>
2020-03-24 11:09:45 +0300

    Add files via upload

---
+++ 105/105/WindowLogin.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace _105
+{
+    /// <summary>
+    /// Логика взаимодействия для WindowLogin.xaml
+    /// </summary>
+    public partial class WindowLogin : Window
+    {
+        public static string NameGamers;
+        public WindowLogin()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (ttt.Text!=null)
+            {
+                NameGamers = ttt.Text;
+                MainWindow mainWin = new MainWindow();
+                mainWin.Show();
+                this.Close();
+            }
+        }
+    }
+}
